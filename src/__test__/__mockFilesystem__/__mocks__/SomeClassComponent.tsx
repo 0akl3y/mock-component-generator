25712497004eb03b81a9e679de06e7e2d906db18
Johannes Eichler <jo.eichler@gmx.de>
2021-02-15 00:27:29 +0100

    Allow imlicit any

--- src/__test__/__mockFilesystem__/__mocks__/SomeClassComponent.tsx
+++ src/__test__/__mockFilesystem__/__mocks__/SomeClassComponent.tsx
@@ -1,4 +1,4 @@
 import React from 'react'
-export const SomeClassComponent = (props) => {
+export const SomeClassComponent = (props: any) => {
   return React.createElement('SomeClassComponent', props)
 }
